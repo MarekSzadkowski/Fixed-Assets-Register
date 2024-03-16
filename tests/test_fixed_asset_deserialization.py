497f424db852d9dd8f87a2fa5ffa2cd6b667af19
Marek Szadkowski <155688591+MarekSzadkowski@users.noreply.github.com>
2024-03-16 16:42:36 +0100

    Minor change at deserialization tests

--- tests/test_fixed_asset_deserialization.py
+++ tests/test_fixed_asset_deserialization.py
@@ -52,19 +52,10 @@ def test_model_deserialization_with_extra_field_added():
     added to the model. Earlier, in the FixedAsset's ConficDict we
     supressed any extra data.
     """
-    model_constrains_with_extra_field_added = {
-        'date': '19.12.2023, 21-12-2023',
-        'name_of_item': 'Dell Latitude 5440 laptop, 8GB RAM, 512GB SSD',
-        'invoice': 'F/174/06/2023',
-        'invoice_date': '30-06-2023',
-        'issuer': '"STATIM LLC" Peter Pan, 123456789',
-        'value': '1537.99',
-        'material_duty_person': 'GDPR',
-        'psp': '0801-D111-00003-01',
-        'cost_center': '1110300',
-        'inventory_number': '487-T-1110300-111100140070',
+    model_constrains_with_extra_field_added = expected_data.copy()
+    model_constrains_with_extra_field_added.update({
         'extra': 'extra field added',
-    }
+    })
     try:
         FixedAsset.model_validate(model_constrains_with_extra_field_added)
     except ValidationError:
